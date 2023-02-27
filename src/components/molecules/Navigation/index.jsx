dd3e61a8b4f9d9f5fc8fa04db3b47aeb1a3001b3
KenjiWorkES <lucas.kenjies@gmail.com>
2023-02-27 18:49:46 -0300

    feat: create navigation component

---
+++ src/components/molecules/Navigation/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const index = () => {
+  return (
+    <nav className="header__nav">
+      <ul className="header__list"></ul>
+    </nav>
+  );
+};
+
+export default index;
--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Navigation } from '@/components/molecules';
 import './styles.scss';
 
 const Header = () => {
@@ -6,6 +7,7 @@ const Header = () => {
     <header className="header">
       <div className="header__container">
         <img className="header__logo" src="/assets/logo.png" alt="Logo Image" />
+        <Navigation />
         <img
           className="header__user"
           src="/assets/user.png"
