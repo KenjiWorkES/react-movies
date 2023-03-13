d92a4086001d8d22bacb904e69f4ea39bbd89768
KenjiWorkES <lucas.kenjies@gmail.com>
2023-03-13 11:31:21 -0300

    feat: create MotionCard

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,1 +1,4 @@
 export { default as NavigationItem } from './NavigationItem';
+export { default as BookmarkButton } from './BookmarkButton';
+export { default as Image } from './Image';
+export { default as MotionInfo } from './MotionInfo';
--- src/components/molecules/MotionCard/index.jsx
+++ src/components/molecules/MotionCard/index.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
 import './styles.scss';
+import { MotionInfo, BookmarkButton, Image } from '../../atoms';
 
-const MotionCard = () => {
-  return <div>MotionCard</div>;
+const MotionCard = ({
+  id,
+  title,
+  year,
+  rating,
+  image,
+  isTrending,
+  category,
+  isBookmarked,
+}) => {
+  const iconId = category === 'Movie' ? 'movies10' : 'tv10';
+
+  const onBookmarkCard = (value) => {};
+
+  return (
+    <figure className={`card ${isTrending && 'card--trending'}`}>
+      <div className="card__imageContainer">
+        <Image src={image} alt={title} />
+        <BookmarkButton
+          initialState={isBookmarked}
+          onBookmark={onBookmarkCard}
+        />
+      </div>
+      <figcaption
+        className={`card__caption ${isTrending && 'card__caption--trending'}`}
+      >
+        <ul className="card__information">
+          <MotionInfo text={year} />
+          <MotionInfo text={category} hasIcon={true} iconId={iconId} />
+          <MotionInfo text={rating} />
+        </ul>
+        <h3 className={`card__title ${isTrending && 'card__title--trending'}`}>
+          {title}
+        </h3>
+      </figcaption>
+    </figure>
+  );
 };
 
 export default index;
