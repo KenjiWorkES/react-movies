9e3047b1feed881970aa47b6a2d8305eb6dcc90c
KenjiWorkES <lucas.kenjies@gmail.com>
2023-03-14 15:05:06 -0300

    feat: create a useSearch to search motions

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,3 +1,34 @@
-const useSearch = () => {
-  return {};
+import { useEffect, useState } from 'react';
+
+const useSearch = (initialArray, sectionTitle, setSectionTitle) => {
+  const [filteredMotions, setFilteredMotions] = useState(initialArray);
+  const [alreadySearched, setAlreadySearched] = useState(false);
+  const title = sectionTitle;
+
+  useEffect(() => {
+    setFilteredMotions(initialArray);
+  }, [initialArray]);
+
+  const searchHandler = (value) => {
+    if (value.trim().length > 0) {
+      const motions = initialArray.value.filter((motion) =>
+        motion.title.toLowerCase().includes(value.toLowerCase())
+      );
+      setFilteredMotions(motions);
+      setSectionTitle(
+        `Found ${filteredMotions.value.length} results for '${value}'`
+      );
+      setAlreadySearched(true);
+    } else {
+      setFilteredMotions(initialArray);
+      setSectionTitle(title);
+      setAlreadySearched(false);
+    }
+  };
+
+  return {
+    filteredMotions: filteredMotions,
+    searchHandler: searchHandler,
+    alreadySearch: alreadySearched,
+  };
 };
